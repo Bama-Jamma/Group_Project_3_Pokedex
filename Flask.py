e90baf44014b009bd99dbdc35f092d2883b69977
Bama-Jamma <dustin.shaddix@gmail.com>
2023-08-07 20:32:23 -0400

    Update Flask.py
    updated flask api to work with variations of all kinds

--- Flask.py
+++ Flask.py
@@ -49,9 +49,6 @@ def get_pokemon_variation_profile(pokedex_number, variation):
 
     return jsonify(pokemon_profile), 200
 
-@app.route('/catch-rate-comparison/<int:pokedex_number>/<variation>', methods=['GET'])
-def get_catch_rate_comparison(pokedex_number, variation):
-    conn = sqlite3.connect('sqlite_database/pokemon_db.sqlite')
 @app.route('/pokemons/', methods=['GET'])
 def get_pokemons():
 
@@ -98,25 +95,72 @@ def get_move_distribution(pokedex_number):
     conn.close()
     return jsonify(move_distribution), 200
 
-@app.route('/stat-comparison/<int:pokedex_number>', methods=['GET'])
-def get_stat_comparison(pokedex_number):
-    conn = sqlite3.connect('sqlite_database\pokemon_db.sqlite')
+
+@app.route('/stat-comparison/<int:pokedex_number>/', methods=['GET'])
+@app.route('/stat-comparison/<int:pokedex_number>/<variation>/', methods=['GET'])
+def get_stat_comparison(pokedex_number, variation=None):
+    conn = sqlite3.connect('sqlite_database/pokemon_db.sqlite')
     cursor = conn.cursor()
 
-    cursor.execute("SELECT * FROM pokemon_data WHERE pokedex_number = ?", (pokedex_number,))
-    all_pokemon_data = cursor.fetchall()
+    if variation is None:
+        cursor.execute("SELECT * FROM pokemon_data WHERE pokedex_number = ?", (pokedex_number,))
+    else:
+        cursor.execute("SELECT * FROM pokemon_data WHERE pokedex_number = ? AND name = ?", (pokedex_number, variation))
 
-    selected_pokemon_data = None
-    for pokemon_data in all_pokemon_data:
-        if variation.lower() in pokemon_data[2].lower():
-            selected_pokemon_data = pokemon_data
-            break
+    selected_pokemon_data = cursor.fetchone()
 
     if not selected_pokemon_data:
+        conn.close()
         return jsonify({"error": "Pokemon not found"}), 404
 
-    catch_rate = selected_pokemon_data[16]
-    pokemon_type = selected_pokemon_data[9]
+    cursor.execute("SELECT AVG(hp), AVG(attack), AVG(defense), AVG(sp_attack), AVG(sp_defense), AVG(speed) FROM pokemon_data")
+    average_stats = cursor.fetchone()
+
+    stat_comparison = {
+        "selected_pokemon": {
+            "name": selected_pokemon_data[1],
+            "hp": selected_pokemon_data[16],
+            "attack": selected_pokemon_data[17],
+            "defense": selected_pokemon_data[18],
+            "sp_attack": selected_pokemon_data[19],
+            "sp_defense": selected_pokemon_data[20],
+            "speed": selected_pokemon_data[21]
+        },
+        "average_stats": {
+            "hp": average_stats[0],
+            "attack": average_stats[1],
+            "defense": average_stats[2],
+            "sp_attack": average_stats[3],
+            "sp_defense": average_stats[4],
+            "speed": average_stats[5]
+        }
+    }
+
+    conn.close()
+    return jsonify(stat_comparison), 200
+
+
+    
+@app.route('/catch-rate-comparison/<int:pokedex_number>', methods=['GET'])
+@app.route('/catch-rate-comparison/<int:pokedex_number>/<variation>', methods=['GET'])
+def get_catch_rate_comparison(pokedex_number, variation=None):
+    conn = sqlite3.connect('sqlite_database/pokemon_db.sqlite')
+    cursor = conn.cursor()
+
+    if variation is None:
+        # If no variation is specified, retrieve the base Pokémon data
+        cursor.execute("SELECT catch_rate, type_1 FROM pokemon_data WHERE pokedex_number = ?", (pokedex_number,))
+    else:
+        # If a variation is specified, retrieve the corresponding variation data
+        cursor.execute("SELECT catch_rate, type_1 FROM pokemon_data WHERE pokedex_number = ? AND name = ?", (pokedex_number, variation))
+
+    pokemon_data = cursor.fetchone()
+
+    if not pokemon_data:
+        return jsonify({"error": "Pokemon not found"}), 404
+
+    catch_rate = pokemon_data[0]
+    pokemon_type = pokemon_data[1]
 
     # Fetch the average catch rate of all Pokémon from the 'pokemon_data' table
     cursor.execute("SELECT AVG(catch_rate) FROM pokemon_data")
@@ -130,13 +174,51 @@ def get_stat_comparison(pokedex_number):
 
     catch_rate_comparison = {
         "pokedex_number": pokedex_number,
-        "name": selected_pokemon_data[2],
         "catch_rate": catch_rate,
         "average_catch_rate_all": average_catch_rate_all,
         "average_catch_rate_same_type": average_catch_rate_same_type
     }
 
     return jsonify(catch_rate_comparison), 200
+
+@app.route('/speed-chart/<int:pokedex_number>/', methods=['GET'])
+@app.route('/speed-chart/<int:pokedex_number>/<pokemon_name>/', methods=['GET'])
+def get_speed_chart(pokedex_number, pokemon_name=None):
+    conn = sqlite3.connect('sqlite_database/pokemon_db.sqlite')
+    cursor = conn.cursor()
+
+    if pokemon_name is None:
+        # If no variation is specified, retrieve the base Pokémon data
+        cursor.execute("SELECT speed FROM pokemon_data WHERE pokedex_number = ?", (pokedex_number,))
+    else:
+        # If a variation is specified, retrieve the corresponding variation data
+        cursor.execute("SELECT speed FROM pokemon_data WHERE pokedex_number = ? AND name = ?", (pokedex_number, pokemon_name))
+
+    selected_speed = cursor.fetchone()
+
+    if not selected_speed:
+        conn.close()
+        return jsonify({"error": "Pokemon not found"}), 404
+
+    cursor.execute("SELECT speed FROM pokemon_data")
+    all_speeds = cursor.fetchall()
+
+    speed_rank = 1
+    for speed in all_speeds:
+        if speed[0] > selected_speed[0]:
+            speed_rank += 1
+
+    conn.close()
+
+    speed_chart = {
+        'pokedex_number': pokedex_number,
+        'selected_speed': selected_speed[0],
+        'speed_rank': speed_rank
+    }
+
+    return jsonify(speed_chart), 200
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
